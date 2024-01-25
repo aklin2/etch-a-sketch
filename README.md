fa7b5cdb3af4f04a01c88e5d1806a7a8a513d778
Alan Lin <lin.alan.k@gmail.com>
2024-01-25 15:07:58 -0800

    made a shitty etch-a-sketch. Styling needs fixing but for the most part its done

---
+++ README.md
@@ -0,0 +1,3 @@
+This is a pretty shitty etch a sketch.
+Made in pure HTML, CSS, and Javascript.
+https://www.theodinproject.com/lessons/foundations-etch-a-sketch
---
+++ index.js
@@ -0,0 +1,44 @@
+let container = document.querySelector(".container");
+
+function makeRow(numSquares = 16) {
+  // Given a number of squares (default 16) creates a row of those squares
+  const row = document.createElement("div");
+  row.className = "row";
+  for (let i = 0; i < numSquares; i++) {
+    let square = document.createElement("p");
+    square.className = "square";
+    row.appendChild(square);
+  }
+  return row;
+}
+
+// Create an n x n matrix (square), where n is the number of rows/columns
+const n = 100;
+for (let i = 0; i < n; i++) {
+  const row = makeRow(n);
+  container.appendChild(row);
+}
+
+// Add an event listener that listens onclick of each square
+const allSquares = Array.from(document.querySelectorAll(".square"));
+for (let square of allSquares) {
+  square.addEventListener("mouseover", function (e) {
+    if (this.style.backgroundColor === "brown") {
+      this.style.backgroundColor = "grey";
+    } else {
+      this.style["background-color"] = "brown";
+    }
+  });
+}
+
+// Reset button
+const btn = document.createElement("button");
+btn.className = "btn";
+btn.innerHTML = "reset";
+btn.addEventListener("click", () => {
+  allSquares.forEach((square) => {
+    square.style.backgroundColor = "grey";
+  });
+});
+
+container.appendChild(btn);
